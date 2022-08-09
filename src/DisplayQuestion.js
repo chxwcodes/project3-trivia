0d9fab28d1cc30a8d744f11cfdfb7d4082294e60
Chow <contact-chxw@gmail.com>
2022-08-09 17:22:40 -0400

    working right/wrong answer select

--- src/DisplayQuestion.js
+++ src/DisplayQuestion.js
@@ -1,12 +1,40 @@
+import { useState } from 'react';
+
 function DisplayQuestion({ questionsArray, answerOptions, currentQuestion, setCurrentQuestion }) {
+    //use state to check user's selected answer
+    const [selectedAnswer, setSelectedAnswer] = useState('');
+    //use state to check user's score
+    const [userScore, setUserScore] = useState(0);
+
+    //a function that sets a value in selectedanswers and checks if its correct and updates the score in userScore
+    const checkAnswer = (userAnswer) => {
+        //put a value in userSelect state
+        setSelectedAnswer(userAnswer);
+
+        if (selectedAnswer === questionsArray[currentQuestion].correct_answer) {
+            //if user is correct, add 1 to score
+            setUserScore(userScore + 1);
+        }
+    }
+
+    //a function that returns a style class
+    const handleSelected = (selectedParam) => {
+        if (selectedAnswer === selectedParam && selectedAnswer === questionsArray[currentQuestion].correct_answer) {
+            return 'correct';
+        } else if (selectedAnswer === selectedParam && selectedAnswer !== questionsArray[currentQuestion].correct_answer){
+            return 'wrong';
+        }
+    }
+
+    //decode the strings
     const decodeString = (string) => {
         return string.replaceAll('&quot;', '"').replaceAll('&#039;', "'");
     }
 
-    //What Pok & eacute; mon &#039;s Base Stat Total does not change when it evolves ?
     return (
         <section className="quizSection">
             <h3>Question {currentQuestion + 1} of 10</h3>
+            <h4>{questionsArray[currentQuestion].category}</h4>
 
             <div className="questionPrompt">
                 <h2>{decodeString(questionsArray[currentQuestion].question)}</h2>
@@ -17,7 +45,13 @@ function DisplayQuestion({ questionsArray, answerOptions, currentQuestion, setCu
                     answerOptions[currentQuestion] !== undefined ?
                         (answerOptions[currentQuestion].map((eachAnswer, index) => {
                             return (
-                                <button key={index}>{decodeString(eachAnswer)}</button>
+                                <button 
+                                    key={index}
+                                    onClick={() => checkAnswer(eachAnswer)}     
+                                    //only apply right/wrong class if there's something in selectedAnswer state
+                                    className={selectedAnswer && handleSelected(eachAnswer)}                
+                                    >                  
+                                {decodeString(eachAnswer)}</button>
                             )
                         })) : null
                 }   
