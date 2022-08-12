d9151433a8d0d633f68069fc0ca7d93b83398c2b
Chow <contact-chxw@gmail.com>
2022-08-12 11:36:36 -0400

    fixed build error with netlify

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
