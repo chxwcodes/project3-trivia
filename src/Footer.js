67bea084ba7f19bd2b12cb764f7eaa9642d2fe77
Chow <contact-chxw@gmail.com>
2022-08-14 18:23:34 -0400

    added footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,8 @@
 function Footer() {
     return (
-        <p className="copyright">Made by <a href="https://chxw.dev/">Chxw</a> ♥ Juno College 2022</p>
+        <footer>
+            <p>Made by <a href="https://chxw.dev/">Chxw</a> ♥ Juno College 2022</p>
+        </footer>
     )
 }
 
